308ecd9ab224ceb5c7be68069ad7c8b419af188c
han <qkrgks378@gmail.com>
2022-07-19 11:51:06 +0900

    feat:로그인/회원가입 완료 페이지 퍼블리싱 #6

--- front/src/components/common/MainNavBar.jsx
+++ front/src/components/common/MainNavBar.jsx
@@ -1,30 +1,53 @@
-import React from 'react';
-import { Link, NavLink} from "react-router-dom";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
-// style import 
+// style import
 import "./MainNavBar.scss";
-import navLogo from "@images/logo/logo_text_green.svg"
+import navLogo from "@images/logo/logo_text_green.svg";
 
-function MainNavBar (){
-    return (
+function MainNavBar() {
+  const activeClassName = active => {
+    const prefix = "left_nav__link fs-16 btn--";
+    return active ? `${prefix}active` : `${prefix}unactive`;
+  };
+
+  return (
     <div className="wrapper flex align-center">
-            <nav id="MainNavBar" className="flex align-center">
-                <Link to="/" className='left_nav__link'>
-                    <img className="logo" title="!213" alt="logoImage" src={navLogo} />
-                </Link>
-                <nav className="left_nav notoBold flex align-center">
-                    <NavLink to="/login">캠핑장</NavLink>
-                    <NavLink to="/board/talk/regist">계획하기</NavLink>
-                    <NavLink to="/board/photo/regist">커뮤니티</NavLink>
-                </nav>
-                <nav className="right_nav notoReg">
-                    <NavLink to="/login">로그인</NavLink>
-                    <NavLink to="/join/finish">회원가입</NavLink>
-                </nav>
-            </nav>
-        </div>
-        //  <img className="logo" alt="logoImage" src={navLogo} />
-    );
+      <nav id="MainNavBar" className="flex align-center">
+        <nav className="left_nav notoBold flex align-center">
+          <Link to="/" className="left_nav__link flex">
+            <img className="logo" title="!213" alt="logoImage" src={navLogo} />
+          </Link>
+          <NavLink
+            className={({ isActive }) => activeClassName(isActive)}
+            to="/login"
+          >
+            캠핑장
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => activeClassName(isActive)}
+            to="/board/talk/regist"
+          >
+            계획하기
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => activeClassName(isActive)}
+            to="/board/photo/regist"
+          >
+            커뮤니티
+          </NavLink>
+        </nav>
+        <nav className="right_nav notoReg">
+          <NavLink className="right_nav__link fs-16" to="/login">
+            로그인
+          </NavLink>
+          <NavLink className="right_nav__link fs-16" to="/join">
+            회원가입
+          </NavLink>
+        </nav>
+      </nav>
+    </div>
+  );
 }
 
 export default MainNavBar;
