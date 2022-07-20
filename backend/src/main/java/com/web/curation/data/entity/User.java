10eaf8a59eb85994ce9d7e20e1033bbb471560aa
joe <gosmf12@naver.com>
2022-07-20 14:01:59 +0900

    setup : 엔티티 추가 #3

---
+++ backend/src/main/java/com/web/curation/data/entity/User.java
@@ -0,0 +1,33 @@
+package com.web.curation.data.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+
+    @Column(nullable = false)
+    private String userName;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String nickname;
+    @Column(nullable = false)
+    private String tel;
+    @Column(nullable = false)
+    private String birth;
+    @Column(nullable = false)
+    private String auth;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String profileImg;
+
+}
