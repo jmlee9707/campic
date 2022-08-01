d2ea0101ad319ab57408cd4c69dbd3c50590fb67
이동명 <aklsjd7380@naver.com>
2022-08-02 04:41:16 +0900

    feat:좋아요, 싫어요, 메인포토, 베스트포토 api

--- front/src/apis/photo.js
+++ front/src/apis/photo.js
@@ -3,15 +3,45 @@ import API from "./index";
 // 포토 등록
 // eslint-disable-next-line import/prefer-default-export
 export const writePhoto = async body => {
-  console.log("111111");
   const res = await API.post("/photo", body); // POST날릴거임
   console.log(res.data);
   return res.data;
 };
 
-
 // 포토상세
 export const getPhotoDetail = async boardId => {
-  const res = await API.get(`/photo/${boardId}`);
+  console.log("1111");
+  const res = await API.get(`/photo/detail/${boardId}`);
+  console.log(res.data);
+  return res.data;
+};
+
+// 좋아요
+export const photoLike = async params => {
+  console.log("22222");
+  console.log(params);
+  const res = await API.post(
+    `/photo/like?boardId=${params.boardId}&email=${params.email}`
+  );
+  return res.data;
+};
+
+// 좋아요 취소
+export const photoDisLike = async params => {
+  const res = await API.delete(
+    `/photo/like?boardId=${params.boardId}&email=${params.email}`
+  );
+  return res.data;
+};
+
+// 포토 메인페이지 컴포넌트
+export const getPhoto = async () => {
+  const res = await API.get("/photo");
+  return res.data;
+};
+
+// 포토 메인페이지 베스트포토 컴포넌트
+export const getBestPhoto = async () => {
+  const res = await API.get("/photo/best");
   return res.data;
 };
