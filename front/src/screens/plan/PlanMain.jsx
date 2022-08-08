0f3f503bffbb1d7a29189129f9a99eb136bd7478
jmlee9707 <jmlee9707@naver.com>
2022-08-08 11:45:25 +0900

    feat : 일정관리 메인 페이지 유저정보 없을시 #21

--- front/src/screens/plan/PlanMain.jsx
+++ front/src/screens/plan/PlanMain.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import "./PlanMain.scss";
 import PlanList from "@components/plan/PlanList";
 
 function PlanMain() {
+  // const dispatch = useDispatch();
+  const userId = useSelector(state => state.user.email);
+  useEffect(() => {
+    console.log(userId);
+  });
   return (
     <div className="container flex justify-center">
       <div className="plan ">
         <div className="plan_title notoBold fs-40">계획하기</div>
-        <div className="plan_coming">
-          <div className="plan_coming_title notoBold fs-28">
-            곧 다가올 캠핑이에요!
+        {userId !== null && (
+          <>
+            <div className="plan_coming">
+              <div className="plan_coming_title notoBold fs-28">
+                곧 다가올 캠핑이에요!
+              </div>
+              <PlanList />
+            </div>
+            <div className="divide" />
+            <div className="plan_past">
+              <div className="plan_past_title notoBold fs-28">
+                지난 캠핑 어떠셨나요?
+              </div>
+              <PlanList className="pastImg" />
+            </div>
+          </>
+        )}
+        {userId === null && (
+          <div className="plan_login flex column align-center justify-center notoMid">
+            <div className="plan_login_txt fs-32">로그인 후 이용하세요!</div>
+            <Link to="/login" className="plan_login_btn fs-18 notoMid">
+              로그인 하러가기
+            </Link>
           </div>
-          <PlanList />
-        </div>
-        <div className="divide" />
-        <div className="plan_past">
-          <div className="plan_past_title notoBold fs-28">
-            지난 캠핑 어떠셨나요?
-          </div>
-          <PlanList className="pastImg" />
-        </div>
+        )}
       </div>
     </div>
   );
