1cd87cc10ff60147d2a5de1298a274a9bbd0a35d
jmlee9707 <jmlee9707@naver.com>
2022-08-08 13:42:50 +0900

    feat : 캠핑 상세 모달창 유저정보 없을시 #19

--- front/src/components/camping/AddPlanModal.jsx
+++ front/src/components/camping/AddPlanModal.jsx
@@ -1,26 +1,21 @@
 import React, { useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 // 달력 api
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 // import { ko } from "date-fns/esm/locale";
 import "./AddPlanModal.scss";
+import { useSelector } from "react-redux";
 import { addPlan } from "../../apis/plan";
 import useOutSideClick from "../../utils/useOutSideClick";
 
 function Modal({ onClose, campId, facltNm }) {
   const navigate = useNavigate();
+  const userId = useSelector(state => state.user.email);
+  console.log(userId);
   const handleClose = () => {
     onClose?.();
   };
-  // const locale = { ko };
-
-  // useEffect(() => {
-  //   const $body = document.querySelector("body");
-  //   $body.style.overflow = "hidden";
-  //   return () => ($body.style.overflow = "auto");
-  // }, []);
-
   const modalRef = useRef(null);
   const tripRef = useRef(""); // 여행 제목 입력 값
   // const email = sessionStorage.getItem("email");
@@ -50,62 +45,85 @@ function Modal({ onClose, campId, facltNm }) {
         ref={modalRef}
         className="modal flex column align-center justify-center"
       >
-        <div className="modal_body flex column align-center justify-center">
-          <div className="modal_body_txt flex column align-center">
-            <div className="modal_body_txt_title notoBold fs-32">{facltNm}</div>
-            <div className="modal_body_txt_days flex align-center notoMid fs-24">
-              날짜 :
-              <div className="flex modal_body_txt_days_select flex ">
-                <DatePicker
-                  selected={startDate}
-                  onChange={date => setStartDate(date)}
-                  selectsStart
-                  startDate={startDate}
-                  endDate={endDate}
-                  // locale={ko}
-                  dateFormat="yyyy/MM/dd"
-                  className="calen notoMid fs-22"
-                />
-                ~
-                <DatePicker
-                  selected={endDate}
-                  onChange={date => setEndDate(date)}
-                  selectsEnd
-                  startDate={startDate}
-                  endDate={endDate}
-                  minDate={startDate}
-                  // locale={ko}
-                  dateFormat="yyyy/MM/dd"
-                  className="calen notoMid fs-22"
+        {userId !== null && (
+          <div className="modal_body flex column align-center justify-center">
+            <div className="modal_body_txt flex column align-center">
+              <div className="modal_body_txt_title notoBold fs-32">
+                {facltNm}
+              </div>
+              <div className="modal_body_txt_days flex align-center notoMid fs-24">
+                날짜 :
+                <div className="flex modal_body_txt_days_select flex ">
+                  <DatePicker
+                    selected={startDate}
+                    onChange={date => setStartDate(date)}
+                    selectsStart
+                    startDate={startDate}
+                    endDate={endDate}
+                    // locale={ko}
+                    dateFormat="yyyy/MM/dd"
+                    className="calen notoMid fs-22"
+                  />
+                  ~
+                  <DatePicker
+                    selected={endDate}
+                    onChange={date => setEndDate(date)}
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={startDate}
+                    // locale={ko}
+                    dateFormat="yyyy/MM/dd"
+                    className="calen notoMid fs-22"
+                  />
+                </div>
+              </div>
+              <div className="modal_body_txt_trip notoMid fs-24">
+                제목 :
+                <input
+                  className="fs-24 notoMid modal_body_txt_trip_input"
+                  ref={tripRef}
+                  placeholder="어떤 여행인가요?"
                 />
               </div>
             </div>
-            <div className="modal_body_txt_trip notoMid fs-24">
-              제목 :
-              <input
-                className="fs-24 notoMid modal_body_txt_trip_input"
-                ref={tripRef}
-                placeholder="어떤 여행인가요?"
-              />
+            <div className="modal_body_btn flex">
+              <button
+                type="button"
+                className="modal_body_btn_add notoBold fs-18 flex align-center justify-center"
+                onClick={movePlan}
+              >
+                일정 추가
+              </button>
+              <button
+                type="button"
+                className="modal_body_btn_close notoBold fs-18 flex align-center justify-center"
+                onClick={handleClose}
+              >
+                창 닫기
+              </button>
             </div>
           </div>
-          <div className="modal_body_btn flex">
-            <button
-              type="button"
-              className="modal_body_btn_add notoBold fs-18 flex align-center justify-center"
-              onClick={movePlan}
-            >
-              일정 추가
-            </button>
-            <button
-              type="button"
-              className="modal_body_btn_close notoBold fs-18 flex align-center justify-center"
-              onClick={handleClose}
-            >
-              창 닫기
-            </button>
+        )}
+        {userId === null && (
+          <div className="modal_login flex column align-center justify-center notoMid">
+            <div className="modal_login_txt fs-24">
+              로그인 후에 이용해 주세요
+            </div>
+            <div className="modal_login_btn flex">
+              <Link to="/login" className="modal_login_btn_move fs-18 notoMid">
+                로그인 하러가기
+              </Link>
+              <button
+                type="button"
+                className="modal_login_btn_close notoBold fs-18 flex align-center justify-center"
+                onClick={handleClose}
+              >
+                창 닫기
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
