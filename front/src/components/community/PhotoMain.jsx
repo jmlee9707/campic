c3b9b76598a2e83671af56771b436446ad75e1e0
이동명 <aklsjd7380@naver.com>
2022-08-08 09:46:28 +0900

    feat : 커뮤니티 메인페이지 포토 부분 개발 #32

--- front/src/components/community/PhotoMain.jsx
+++ front/src/components/community/PhotoMain.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./PhotoMain.scss";
 
-import friends from "@images/friend.jpeg";
-
-function PhotoMain() {
+// eslint-disable-next-line react/prop-types
+function PhotoMain({ blobFile, boardId }) {
+  const navigate = useNavigate();
+  const onClickImg = () => {
+    navigate(`/board/photo/detail/${boardId}`);
+  };
   return (
-    <div className="MainPhoto">
-      <img src={friends} alt="베스트이미지" />
-    </div>
+    <button type="button" onClick={onClickImg}>
+      <div className="MainPhoto">
+        <img src={[blobFile]} alt="베스트이미지" />
+      </div>
+    </button>
   );
 }
 
