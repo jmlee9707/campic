fe45795e4fad8c0f5fb5432b5b3d364c22e0b271
이동명 <aklsjd7380@naver.com>
2022-08-11 01:09:25 +0900

    fix : 포토 상세 네비게이트 추가

--- front/src/components/mypage/MyPhotoCard.jsx
+++ front/src/components/mypage/MyPhotoCard.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./MyPhotoCard.scss";
 
 // import coco from "@images/coco.jpeg";
 
-function MyPhotoCard({blobFile}) {
+function MyPhotoCard({blobFile, boardId}) {
+  const navigate = useNavigate();
+  const onClickPhoto = () => {
+    navigate(`/board/photo/detail/${boardId}`)
+  }
   return (
-    <div className="photoCard">
-      <img src={[blobFile]} alt="내 이미지" />
+    <div className="photoCard flex">
+      <button type="button" onClick={onClickPhoto}>
+        <img src={[blobFile]} alt="내 이미지" />
+      </button>
     </div>
   );
 }
