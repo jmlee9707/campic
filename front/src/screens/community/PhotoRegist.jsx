724975080fc7d85f44d1ce521b65f8f773c246db
이동명 <aklsjd7380@naver.com>
2022-08-10 21:57:42 +0900

    fix : 포토 등록 사용자정보 불러오기

--- front/src/screens/community/PhotoRegist.jsx
+++ front/src/screens/community/PhotoRegist.jsx
@@ -3,17 +3,20 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useRef, useState } from "react";
 import "./PhotoRegist.scss";
-import imageCompression from "browser-image-compression"
+import imageCompression from "browser-image-compression";
 import { useNavigate } from "react-router-dom";
 import { ReactComponent as Camera } from "@images/logo/logo_photo_black.svg";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { useBeforeunload } from "react-beforeunload";
+import { selectProfile } from "../../store/user";
 import { writePhoto } from "../../apis/photo"; // 포토등록 api
 
 function PhotoRegist() {
-  // 유저정보 파악  
+  // 유저정보 파악
+  const profile = useSelector(selectProfile);
+  // 로그인 여부 파악
   const userId = useSelector(state => state.user.email);
-  const nickname = useSelector(state => state.user.nickname);
+  // const profileImg = useSelector(state => state.user.profileImg)
   const navigate = useNavigate(); // 네비게이트, 작성 시 포토 상세페이지로 이동
   // 카메라 이미지에 파일 인풋 달기
   const photoInput = useRef(); // 포토ref
@@ -38,13 +41,13 @@ function PhotoRegist() {
     actionImgCompress(photoInput.current.files[0]);
   };
 
-  const actionImgCompress = async (fileImage) => {
-    console.log("압축할게")
+  const actionImgCompress = async fileImage => {
+    console.log("압축할게");
 
     const options = {
       maxSizeMB: 0.2,
       maxWidthOrHeight: 720,
-      useWebWorker: true,
+      useWebWorker: true
     };
     try {
       const compressedFile = await imageCompression(fileImage, options);
@@ -52,12 +55,12 @@ function PhotoRegist() {
       const reader = new FileReader();
       reader.readAsDataURL(compressedFile);
       reader.onloadend = () => {
-      // 변환 완료!
+        // 변환 완료!
         const base64data = reader.result;
-        console.log("변경끝")
-        console.log(base64data)
+        console.log("변경끝");
+        console.log(base64data);
 
-      // formData 만드는 함수
+        // formData 만드는 함수
         // eslint-disable-next-line no-use-before-define
         handlingDataForm(base64data);
       };
@@ -66,13 +69,10 @@ function PhotoRegist() {
     }
   };
 
-  useBeforeunload((event) => event.preventDefault());
-
-
   const handlingDataForm = async dataURI => {
     // dataURL 값이 data:image/jpeg:base64,~~~~~~~ 이므로 ','를 기점으로 잘라서 ~~~~~인 부분만 다시 인코딩
     const byteString = atob(dataURI.split(",")[1]);
-  
+
     // Blob를 구성하기 위한 준비, 이 내용은 저도 잘 이해가 안가서 기술하지 않았습니다.
     const ab = new ArrayBuffer(byteString.length);
     const ia = new Uint8Array(ab);
@@ -84,14 +84,15 @@ function PhotoRegist() {
       type: "image/jpeg"
     });
     const file = new File([blob], "image.jpg");
-  
+
     // 위 과정을 통해 만든 image폼을 FormData에 넣어줍니다.
     // 서버에서는 이미지를 받을 때, FormData가 아니면 받지 않도록 세팅해야합니다.
     const formData = new FormData();
-    
+
     // 필요시 더 추가합니다.
-    formData.append("nickname", nickname);
-    formData.append("email", userId);
+    formData.append("nickname", profile.nickname);
+    formData.append("profileImg", profile.profileImg);
+    formData.append("email", profile.userId);
     formData.append("content", textareaRef.current.value);
     formData.append("hashtag", tagRef.current.value);
     formData.append("fileName", "baek");
@@ -101,8 +102,7 @@ function PhotoRegist() {
     for (const value of formData.values()) {
       console.log(value);
     }
-    
-  
+
     try {
       const res = await writePhoto(formData);
       if (res.message === "success") {
@@ -113,81 +113,78 @@ function PhotoRegist() {
     }
   };
 
+  useBeforeunload(event => event.preventDefault());
 
-  
   return (
     <div className="container flex">
       {userId !== null && (
         <div className="regist">
-        <div className="regist_title notoBold fs-32">사진 등록하기</div>
-        <div className="regist_content flex">
-          {/* 사진 업로드 박스 */}
-          <div
-            className="regist_content_img flex align-center justify-center"
-            onClick={handleclick}
-          >
-            {!fileImage && <Camera className="camera" fill="#DBDBDB" />}
-            {!fileImage && (
-              <div className="regist_content_img_sub fs-28 notoBold">
-                Upload
+          <div className="regist_title notoBold fs-32">사진 등록하기</div>
+          <div className="regist_content flex">
+            {/* 사진 업로드 박스 */}
+            <div
+              className="regist_content_img flex align-center justify-center"
+              onClick={handleclick}
+            >
+              {!fileImage && <Camera className="camera" fill="#DBDBDB" />}
+              {!fileImage && (
+                <div className="regist_content_img_sub fs-28 notoBold">
+                  Upload
+                </div>
+              )}
+              {fileImage && (
+                <div className="regist_content_img_priv">
+                  <img alt="sample" src={fileImage} />
+                </div>
+              )}
+              <input
+                type="file"
+                multiple="multiple"
+                encType="multipart/form-data"
+                accept="image/jpg, image.jpeg, image.png"
+                ref={photoInput}
+                style={{ display: "none" }}
+                name="imgFile"
+                id="imgFile"
+                onChange={saveFileImage}
+              />
+            </div>
+
+            <div className="regist_content_text">
+              {/* 사진 설명 박스 */}
+
+              <textarea
+                type="textarea"
+                placeholder="사진에 대해 설명해주세요."
+                className="regist_content_text_input_box notoMid fs-20"
+                ref={textareaRef}
+              />
+
+              {/* 태그 입력 박스 */}
+
+              <input
+                ref={tagRef}
+                type="text"
+                placeholder="#태그입력"
+                className="regist_content_text_tag flex notoMid fs-20"
+                // maxLength={30} // 일단 maxlength지정 해둠
+              />
+
+              {/* 등록하기 버튼 */}
+              <div className="regist_content_text_box flex">
+                <button
+                  type="button"
+                  className="regist_content_text_btn notoBold fs-24"
+                  onClick={submit}
+                >
+                  등록하기
+                </button>
               </div>
-            )}
-            {fileImage && (
-              <div className="regist_content_img_priv">
-                <img alt="sample" src={fileImage} />
-              </div>
-            )}
-            <input
-              type="file"
-              multiple="multiple"
-              encType="multipart/form-data"
-              accept="image/jpg, image.jpeg, image.png"
-              ref={photoInput}
-              style={{ display: "none" }}
-              name="imgFile"
-              id="imgFile"
-              onChange={saveFileImage}
-            />
-          </div>
-
-          <div className="regist_content_text">
-            {/* 사진 설명 박스 */}
-
-            <textarea
-              type="textarea"
-              placeholder="사진에 대해 설명해주세요."
-              className="regist_content_text_input_box notoMid fs-20"
-              ref={textareaRef}
-            />
-
-            {/* 태그 입력 박스 */}
-
-            <input
-              ref={tagRef}
-              type="text"
-              placeholder="#태그입력"
-              className="regist_content_text_tag flex notoMid fs-20"
-              // maxLength={30} // 일단 maxlength지정 해둠
-            />
-
-            {/* 등록하기 버튼 */}
-            <div className="regist_content_text_box flex">
-              <button
-                type="button"
-                className="regist_content_text_btn notoBold fs-24"
-                onClick={submit}
-              >
-                등록하기
-              </button>
             </div>
           </div>
         </div>
-      </div>
       )}
-      {userId === null && (
-        window.alert("로그인 후 이용해주세요!!")
-        
-        )}
+      {userId === null && window.alert("로그인 후 이용해주세요!!")}
     </div>
   );
 }
