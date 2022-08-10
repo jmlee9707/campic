63559a1f2e2f0144746e907be9f4b76fe25c4580
antipodal <antipodal@naver.com>
2022-08-10 17:07:23 +0900

    feat: notfound와 비로그인시 접근 제한

--- front/src/routers/Router.jsx
+++ front/src/routers/Router.jsx
@@ -35,12 +35,26 @@ import CampingDetail from "@screens/camping/CampingDetail";
 import PlanMain from "@screens/plan/PlanMain";
 import PlanDetail from "@screens/plan/PlanDetail";
 
+// others
+import NotFound from "@screens/NotFound";
+import PlzLogin from "@screens/PlzLogin";
+
 // Community
-import Community from "./Community";
+// import Community from "./Community";
+import CommunityNavBar from "@components/community/CommunityNavBar";
+import CommunityMain from "@screens/community/CommunityMain";
+import PhotoHome from "@screens/community/PhotoHome";
+// import TalkHome from "@screens/community/TalkHome"
+// import PhotoRegist from "@screens/community/PhotoRegist";
+import TalkRegist from "@screens/community/TalkRegist";
+import PhotoDetail from "@screens/community/PhotoDetail";
+import PhotoModify from "@screens/community/PhotoModify";
+// import TalkDetail from "@screens/community/TalkDetail";
+// import PhotoModify from "../screens/community/PhotoModify";
 
 function Router() {
 
-  const flag = useSelector(state => state.user.email);
+  const isLogined = useSelector(state => state.user.email);
 
 
   return (
@@ -74,33 +88,74 @@ function Router() {
         </Route>
 
         {/* info edit */}
-        <Route path="/infoedit/*">
-          <Route index element={<InfoEdit />} />
-          <Route path="pwch" element={<PwCh />} />
-          <Route path="pwedit" element={<PwEdit />} />
-        </Route>
+        { isLogined &&
+          <Route path="/infoedit/*">
+            <Route index element={<InfoEdit />} />
+            <Route path="pwch" element={<PwCh />} />
+            <Route path="pwedit" element={<PwEdit />} />
+          </Route>
+        }
+        { !isLogined &&
+          <Route path="/infoedit/*">
+            <Route path="*" element={<PlzLogin />} />
+          </Route>
+        }
+
 
         {/* Drop */}
+        { isLogined &&
+          <Route path="/drop/*">
+            <Route index element={<Drop />} />
+            <Route path="finish" element={<DropFinish />} />
+          </Route>
+        }
+        { !isLogined &&
         <Route path="/drop/*">
-          <Route index element={<Drop />} />
-          <Route path="finish" element={<DropFinish />} />
+          <Route path="*" element={<PlzLogin />} />
         </Route>
+        }
+
 
         {/* community */}
-        {/* <CommunityNavBar /> */}
-        <Route path="/board/*" element={<Community />} />
+        { isLogined && <CommunityNavBar/> &&
+          <Route path="/board/*">
+            <Route index element={<CommunityMain />} />
+            <Route path="photo/home" element={<PhotoHome />} />
+            {/* <Route path="talk/home" element={<TalkHome />} /> */}
+            <Route path="talk/regist" element={<TalkRegist />} />
+            <Route path="photo/detail/:id" element={<PhotoDetail />} />
+            {/* <Route path="talk/detail/:id" element={<TalkDetail />} /> */}
+            <Route path="photo/modi/:id" element={<PhotoModify />} />
+            {/* <Route path="photo/detail" element={<PhotoDetail />} /> */}
+            {/* <Route path="talk/detail" element={<TalkDetail />} /> */}
+          </Route>
+        }
+        { !isLogined && 
+          <Route path="/board/*">
+            <Route index element={<CommunityMain />} />
+            <Route path="*" element={<PlzLogin />} />
+          </Route>
+        }
           
           
-
+        {/* camping */}
         <Route path="/camping/*">
           <Route index element={<CampingMain />} />
           <Route path="detail/:id" element={<CampingDetail />} />
         </Route>
-        {flag &&
-        <Route path="/plan/*">
+
+        {/* plan */}
+        { isLogined &&
+          <Route path="/plan/*">
           <Route index element={<PlanMain />} />
           <Route path="detail/:id" element={<PlanDetail />} />
-        </Route>
+          </Route>
+        }
+        { !isLogined && 
+          <Route path="/plan/*">
+            <Route index element={<PlanMain />} />
+            <Route path="*" element={<PlzLogin />} />
+          </Route>
         }
         {/* <Route path="/plan/*">
           <Route index element={<PlanMain />} />
@@ -108,16 +163,23 @@ function Router() {
         </Route> */}
 
         {/* mypage */}
-        <Route path="/mypage/*">
+        { isLogined && 
+          <Route path="/mypage/*">
           <Route path="myfeed" element={<MyFeed />} />
           <Route path="drop" element={<Drop />} />
           <Route path="drop/finish" element={<DropFinish />} />
           <Route path="pwch" element={<PwCh />} />
           <Route path="pwch/edit" element={<PwEdit />} />
           <Route path="info/edit" element={<InfoEdit />} />
-        </Route>
+          </Route>
+        }
+        { !isLogined && 
+          <Route path="/mypage/*">
+            <Route path="*" element={<PlzLogin />} />
+          </Route>
+        }
+        <Route path="*" element={<NotFound />} />
       </Routes>
-
     </>
   );
 }
