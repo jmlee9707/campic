113b40a2edb01b233c555a60ab9d9d2bf6f6e73d
김수빈 <chfhrdl0001@naver.com>
2022-08-11 10:28:02 +0900

    fix : camp 검색 주석 삭제

--- backend/src/main/java/com/web/curation/service/CampService.java
+++ backend/src/main/java/com/web/curation/service/CampService.java
@@ -46,7 +46,12 @@ public class CampService{
         List<TotalCampList> totalCampLists = new ArrayList<>();
 
         if(searchList.getKeyword()==null && searchList.getTags().isEmpty() && searchList.getGugun()==null && searchList.getSido()==null){
-            totalCampLists = campRepository.findAll();
+            if(searchList.getArrange() == 0) {
+                totalCampLists = campRepository.findAll(Sort.by(Sort.Direction.ASC,"facltNm"));
+            }else{
+                totalCampLists = campRepository.findAll();
+            }
+
         }else{
             totalCampLists = campRepository.findAll(searchCamps(searchList));
         }
@@ -104,57 +109,6 @@ public class CampService{
     }
 
 
-
-
-//    public List<CampDto.CampList> filterCampList(SearchListDto.SearchList searchList){
-//        List<TotalCampList> totalCampLists = new ArrayList<>();
-//        List<CampDto.CampList> filterCampList = new ArrayList<>();
-//        /* 전체 리스트 */
-//        if(searchList.getKeyword()==null && searchList.getTags()==null &&
-//                searchList.getGugun()==null && searchList.getSido()==null){
-//            totalCampLists = getAllCamps();
-//        }
-//        /* 키워드 검색 필터링 */
-//        if (searchList.getKeyword() != null){
-//            List<TotalCampList> kwSid = keywordSearchCampList(searchList.getKeyword());
-//            for (TotalCampList k : kwSid){
-//                totalCampLists.remove(k);
-//            }
-//        }
-//
-//        /* 지역 검색 필터링 */
-//        if (searchList.getSido() != null && searchList.getGugun() != null){
-//            List<TotalCampList> rgSlist = regionSearchCampList(searchList.getSido(), searchList.getGugun());
-//            for (TotalCampList r : rgSlist){
-//                totalCampLists.remove(r);
-//            }
-//        }
-//
-//        /* 태그 겁색 필터링 */
-//        if (searchList.getTags() != null){
-//            List<TotalCampList> tagSearchCampList = tagSearchCampList(searchList.getTags());
-//            for (TotalCampList ts : tagSearchCampList){
-//                totalCampLists.add(ts);
-//            }
-//        }
-//
-//        Pageable pageable = PageRequest.of(searchList.getPage(), 10);
-//        final int start = (int)pageable.getOffset();
-//        final int end = Math.min((start + pageable.getPageSize()), totalCampLists.size());
-//        Page<TotalCampList> ptl = new PageImpl<>(totalCampLists.subList(start,end),pageable ,totalCampLists.size());
-//
-//        for (TotalCampList cl : ptl){
-//            CampDto.CampList tcl = new CampDto.CampList(cl);
-//            filterCampList.add(tcl);
-//        }
-//
-//        return filterCampList;
-//    }
-
-
-
-
-
     /* campList READ */
     @Transactional(readOnly = true)
     public List<CampDto.CampList> getAllCamps(int page) {
@@ -183,38 +137,7 @@ public class CampService{
         return new CampDto.CampDetail(totalCampList);
     }
 
-    /* camp 키워드 검색 결과 리스트 READ */
-    @Transactional(readOnly = true)
-    public List<TotalCampList> keywordSearchCampList(String keyword){
-        List<TotalCampList> kwSearchCampList = campRepository.findByFacltNmNotContains(keyword);
-        return kwSearchCampList;
-    }
-
-    /* camp 지역 검색 결과 리스트 READ */
-    @Transactional(readOnly = true)
-    public List<TotalCampList> regionSearchCampList(String doname, String sigungu){
-        List<TotalCampList> regionSearchCampList = campRepository.findByDoNmAndSigunguNmStartsWith(doname,sigungu);
-        return regionSearchCampList;
-    }
-
-    /* camp 태그 검색 결과 리스트 READ */
-    @Transactional(readOnly = true)
-    public List<TotalCampList> tagSearchCampList(List<String> taglist){
-        List<TagDto.SearchedTag> selecteds = tagRepository.findDistinctByAndHashtagIn(taglist);
-
-        List<TotalCampList> tagSearchCampList = new ArrayList<>();;
-        for (TagDto.SearchedTag s : selecteds){
-            TotalCampList camp = campRepository.getByCampId(s.getCampId());
-            System.out.println(camp.getCampId());
-            tagSearchCampList.add(camp);
-        }
-
-        return tagSearchCampList;
-    }
-
-
-
-    /* CREATE */
+     /* CREATE */
     @Transactional
     public int save(ScheduleDto.Request dto, String email, int campId) {
         /* User 정보를 가져와 dto에 담아준다. */
