016c0cb86ecbc29620c4cda790f5f648c2a4e31d
이동명 <aklsjd7380@naver.com>
2022-08-18 15:21:22 +0900

    fix : 무한스크롤 수정

--- front/src/components/community/CommunityPhotoList.jsx
+++ front/src/components/community/CommunityPhotoList.jsx
@@ -2,38 +2,29 @@ import React, { useState, useEffect } from "react";
 import { v4 } from "uuid";
 import Loading from "@components/common/Loading";
 import { useInView } from "react-intersection-observer";
-import { useDispatch, useSelector } from "react-redux";
-import { setPhotoList } from "../../store/photo";
+import LastList from "@components/common/LastList";
 import CommunityPhotoCard from "./CommunityPhotoCard";
+
 import { getPhoto } from "../../apis/photo";
 
 function CommunityPhotoList() {
-  // const [photoList, setPhotoList] = useState([]); // 불러온 데이터 저장
-  const photoList = useSelector(state => state.photo.photoList);
+  const [photoList, setPhotoList] = useState([]); // 불러온 데이터 저장
 
   const [page, setPage] = useState(0); // 현재페이지
   const [loading, setLoading] = useState(false);
   const [ref, inView] = useInView();
-  const dispatch = useDispatch();
-  const isEnd = useSelector(state => state.photo.isEnd);
-  // async function getPhotoList() {
-  //   console.log(11111)
-  //   const res = await getPhoto(page);
-  //   console.log(22222)
-  //   useDispatch(setPhotoList({photoList:res}))
-  //   setLoading(false);
-  // }
+  const [last, setLast] = useState(false);
 
-  const getPhotoList = async () => {
-    console.log(11111);
+  async function getPhotoList() {
     const res = await getPhoto(page);
-    console.log(22222);
-    dispatch(setPhotoList({ photoList: res }));
+    setPhotoList([...photoList, ...res]);
     setLoading(false);
-  };
-
+    if (res.length >= 0 && res.length < 15) {
+      setLast(true);
+    }
+  }
   useEffect(() => {
-    if (!!inView && !loading) {
+    if (inView && !loading) {
       setLoading(true);
       setPage(page + 1);
       getPhotoList();
@@ -66,19 +57,8 @@ function CommunityPhotoList() {
             />
           )
         )}
-      {!isEnd && loading ? (
-        <Loading />
-      ) : (
-        <div ref={ref} className="observer">
-          여기
-        </div>
-      )}
-      {isEnd && (
-        <div ref={ref} className="observer">
-          end
-        </div>
-      )}
-      {/* <div ref={ref} className="observer" /> */}
+      {!last && loading ? <Loading /> : <div ref={ref} className="observer" />}
+      {last && <LastList />}
     </div>
   );
 }
