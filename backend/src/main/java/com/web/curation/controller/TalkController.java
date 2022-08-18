a8c1017b3558d6c7b6122adaa73f8c81efa82542
김수빈 <chfhrdl0001@naver.com>
2022-08-18 17:59:37 +0900

    fix : talk 수정

--- backend/src/main/java/com/web/curation/controller/TalkController.java
+++ backend/src/main/java/com/web/curation/controller/TalkController.java
@@ -84,40 +84,6 @@ public class TalkController {
             e2.printStackTrace();
         }
 
-//        // 이미지 파일이 아닐 때
-//        if(file.getContentType().startsWith("image") == false){
-//            LOGGER.warn("this file is not image type");
-//            resultMap.put("message", FAIL);
-//            status = HttpStatus.BAD_REQUEST;
-//
-//            return new ResponseEntity<>(resultMap, status);
-//        }
-//
-//        //브라우저에 따라 업로드하는 파일의 이름은 전체경로일 수도 있고(Internet Explorer),
-//        //단순히 파일의 이름만을 의미할 수도 있습니다.(chrome browser)
-//        String originalName = file.getOriginalFilename();//파일명:모든 경로를 포함한 파일이름
-//        String fileName = originalName.substring(originalName.lastIndexOf("//") + 1);
-//
-//        LOGGER.info("fileName" + fileName);
-//
-//        //UUID
-//        String uuid = UUID.randomUUID().toString();
-//        //저장할 파일 이름 중간에 "_"를 이용하여 구분
-//        String saveName = uploadPath + File.separator + File.separator + uuid + "_" + fileName;
-//
-//        Path savePath = Paths.get(saveName);
-//        //Paths.get() 메서드는 특정 경로의 파일 정보를 가져옵니다.(경로 정의하기)
-//
-//        try {
-//            file.transferTo(savePath);
-//            //uploadFile에 파일을 업로드 하는 메서드 transferTo(file)
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            //printStackTrace()를 호출하면 로그에 Stack trace가 출력됩니다.
-//        }
-//
-//        talkDto.setFilePath(savePath.toString());
-
         int result = talkService.writeTalk(talkDto);
 
         if (result != 0) {
@@ -134,25 +100,20 @@ public class TalkController {
 
 
     @PutMapping
-    public ResponseEntity<String> updateTalk(TalkDto talkDto, MultipartFile file) {
+    public ResponseEntity<String> updateTalk(TalkDto talkDto) {
         LOGGER.info("updateTalk - 호출");
-//
-//        talkDto.setFilePath(file.getOriginalFilename());
-//        if (talkService.updateTalk(talkDto)) {
-//            return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
-//        }
-//        return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
-        String fileName = file.getOriginalFilename();
-        talkDto.setFileName(fileName);
-        byte[] bytes;
 
-        try{
-            bytes = file.getBytes();
-            talkDto.setSaveFile(bytes);
-            LOGGER.info("bytes 파일 {}", bytes.toString().substring(0,11));
-        }  catch (IOException e2){
-            e2.printStackTrace();
-        }
+//        String fileName = file.getOriginalFilename();
+//        talkDto.setFileName(fileName);
+//        byte[] bytes;
+//
+//        try{
+//            bytes = file.getBytes();
+//            talkDto.setSaveFile(bytes);
+//            LOGGER.info("bytes 파일 {}", bytes.toString().substring(0,11));
+//        }  catch (IOException e2){
+//            e2.printStackTrace();
+//        }
 
         if (talkService.updateTalk(talkDto)) {
             return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
